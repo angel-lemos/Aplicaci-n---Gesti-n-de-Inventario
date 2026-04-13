31f72ba1ab36bcafe585d48d37c3f7c49173fe3c
angel.lemos <angel.lemos@redqualitas.edu.uy>
2026-04-13 10:18:18 -0300

    Se agregó el CRUD de Compras y Productos, además  se crearon los templates de estos

--- Gestion_inventario/Gestion_inventario/urls.py
+++ Gestion_inventario/Gestion_inventario/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -23,6 +23,7 @@ urlpatterns = [
     
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('ventas/', auth_views.LoginView.as_view(template_name='ventas.html'), name='ventas'),
     
+    # URLs de la app inventario
+    path('', include('inventario.urls')),
 ]
